0f608650f8a50a66ae586d14fbd8f669abea7b20
Commeci <cjtkemfldj@naver.com>
2024-10-24 17:17:54 +0900

    Feat: Footer 완성

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,3 +1,8 @@
+import { Logo } from "@components/Common/Logo";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return <div className="bg-black px-16 py-7">
+    <Logo/>
+    <p className="text-white-sub mt-8">ⓒ2024. deview all rights reserved</p>
+  </div>;
 }
