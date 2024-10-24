2c23bc9af45978f0f8fb24ae55fc470da05ecd33
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-24 16:55:13 +0900

    Feat: Logo Component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,12 @@
+import { Logo } from "@components/Common/Logo";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="mx-auto max-w">
+      <Logo />
+      <input name="id" type="email" />
+      <button className="primary-btn">asd</button>
+      <button className="text-btn">비밀번호 찾기</button>
+    </div>
+  );
 }
