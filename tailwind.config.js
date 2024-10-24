09fa361427fa788394359dedbec2558d53a0ed76
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-24 12:25:50 +0900

    Style: Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,40 @@ export default {
       fontFamily: {
         Pretendard: ["Pretendard"]
       }
+    },
+    colors: {
+      primary: "#8889FF",
+      secondary: "#6668FF",
+      white: {
+        pure: "#FFFFFF",
+        sub: "#F6F7FF"
+      },
+      black: "#1E1E1E",
+      gray: "#8C8C8C",
+      lightgray: "#CCCCCC",
+      pink: "#FF7C7A",
+      purple: "#8469E3",
+      lightpurple: "#D8D8FF",
+      skyblue: "#50C6E1",
+      lightgreen: "#44CF86",
+      yellow: "#EEDD60",
+      lightyellow: "#FFF8C9"
     }
   },
-  plugins: []
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        ".custom": {
+          "@apply mx-auto lg:max-w-[1140px] md:max-w-[720px] sm:max-w-[576px]": ""
+        }
+      });
+    },
+    ({ addComponents }) => {
+      addComponents({
+        ".custom-comp": {
+          "@apply flex": ""
+        }
+      });
+    }
+  ]
 };
