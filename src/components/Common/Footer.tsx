af5848be2d21bfc2bd198dcd22eeab55becbc529
Commeci <cjtkemfldj@naver.com>
2024-10-25 15:17:17 +0900

    Style: Footer 스타일 코드 변경

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -2,9 +2,9 @@ import { Logo } from "@components/Common/Logo";
 
 export default function Footer() {
   return (
-    <div className="bg-black px-16 py-7">
+    <footer className="h-40 bg-black px-16 py-7">
       <Logo />
       <p className="mt-8 text-12 text-white-sub">ⓒ2024. deview all rights reserved</p>
-    </div>
+    </footer>
   );
 }
