4ce6dde24395235536cb6529e5f5fe3c6ccdb267
Commeci <cjtkemfldj@naver.com>
2024-10-25 19:16:56 +0900

    Feat: InfoContent컴포넌트 기능 추가

--- src/components/MyPage/InfoContent.tsx
+++ src/components/MyPage/InfoContent.tsx
@@ -1,14 +1,58 @@
+import { IdInput } from "@components/Common/IdInput";
+import { NameInput } from "@components/Common/NameInput";
+import { PasswordInput } from "@components/Common/PasswordInput";
+import { CheckPassword } from "@components/MyPage/CheckPassword";
+import { Radio } from "@components/Common/Radio";
+import { useState } from "react";
+
 type InfoContentProps = {
-  data: { name?: string; id?: string; password?: string; department?: string };
+  data: {
+    _id?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+    team?: string;
+    created_at?: string;
+  };
 };
 
 export const InfoContent = ({ data }: InfoContentProps) => {
+  const [isVerified, setIsVerified] = useState(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const updatedData = {
+      username: formData.get("name") as string,
+      password: formData.get("password") as string,
+      team: formData.get("group") as string
+    };
+    // TODO: 업데이트 처리
+  };
+
+  // TODO: Ratio 컴포넌트 공통으로 이동
   return (
-    <div>
-      <p>name: {data.name ?? ""}</p>
-      <p>id: {data.id ?? ""}</p>
-      <p>password: {data.password ?? ""}</p>
-      <p>department: {data.department ?? ""}</p>
+    <div className="mx-auto max-w-[768px] py-4">
+      {!isVerified ? (
+        <CheckPassword onSuccess={() => setIsVerified(true)} correctPassword={data.password ?? ""} />
+      ) : (
+        <form className="" onSubmit={onSubmit}>
+          <IdInput defaultValue={data.email} disabled={true} />
+          <NameInput defaultValue={data.username} />
+          <PasswordInput defaultValue={data.password} />
+
+          <div className="mb-5 flex items-center justify-between gap-10">
+            <Radio id="student" name="group" text="학생" value="학생" defaultChecked={data.team === "학생"} />
+            <Radio id="seeker" name="group" text="취준생" value="취준생" defaultChecked={data.team === "취준생"} />
+            <Radio id="developer" name="group" text="개발자" value="개발자" defaultChecked={data.team === "개발자"} />
+            <Radio id="etc" name="group" text="기타" value="기타" defaultChecked={data.team === "기타"} />
+          </div>
+          <button type="submit" className="primary-btn p-7">
+            저장
+          </button>
+        </form>
+      )}
     </div>
   );
 };
