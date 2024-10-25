15a9da5393a52ef3fff995f419cee309a3330704
Commeci <cjtkemfldj@naver.com>
2024-10-25 19:13:28 +0900

    Feat: NameInput 컴포넌트 props 처리 추가

---
+++ src/components/Common/NameInput.tsx
@@ -0,0 +1,14 @@
+import { Input } from "@components/Common/Input";
+import { AUTH_INPUT_VALIDATION } from "@constants/authInputValidation";
+export const NameInput = ({ defaultValue }: { defaultValue?: string }) => {
+  return (
+    <Input
+      name="name"
+      text="이름"
+      errorMessage={AUTH_INPUT_VALIDATION.name.errorMessage}
+      pattern={AUTH_INPUT_VALIDATION.name.regexp}
+      required
+      defaultValue={defaultValue}
+    />
+  );
+};
