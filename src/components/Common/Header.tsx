b93daf9b8ca7b3e332477bad1bec736f7aa3189e
jaja <dkwjdals35@gmail.com>
2024-10-25 15:10:01 +0900

    Feat: Mobile  Header UI

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { Logo } from "@components/Common/Logo";
 import { SearchBar } from "@components/Common/SearchBar";
 import { Link } from "react-router-dom";
+import { HeaderMenu } from "@components/Common/HeaderMenu";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className="shadow">
-      <header className="mx-auto flex h-28 max-w items-center justify-between p-4">
-        <div className="flex items-center">
-          <Link to="/">
-            <Logo />
-          </Link>
-
-          <nav className="ml-8 flex space-x-8">
+      <header className="mx-auto flex h-36 max-w flex-col items-center justify-between p-4 md:h-28 md:flex-row">
+        <div className="flex w-full items-center md:w-auto">
+          <button className="mr-4 md:hidden" onClick={onClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="13" fill="none">
+              <path fill="#000" d="M0 12.5v-2h18v2H0Zm0-5v-2h18v2H0Zm0-5v-2h18v2H0Z" />
+            </svg>
+          </button>
+          <div className="flex-grow text-center">
+            <Link to="/" className="flex justify-center">
+              <Logo />
+            </Link>
+          </div>
+          <nav className="hidden md:ml-8 md:flex md:space-x-8">
             <Link className="text-lg text-gray" type="button" to="/post">
               게시글
             </Link>
@@ -20,10 +34,10 @@ export const Header = () => {
             </Link>
           </nav>
         </div>
-        <div className="mx-16 flex-grow">
+        <div className="w-full md:mx-16 md:w-auto md:flex-grow">
           <SearchBar />
         </div>
-        <div className="flex space-x-2">
+        <div className="hidden md:flex md:space-x-2">
           <Link className="lightgray-btn h-10 w-24 p-1 flex-center" type="button" to="/login">
             로그인
           </Link>
@@ -32,6 +46,13 @@ export const Header = () => {
           </Link>
         </div>
       </header>
+
+      {isMenuOpen && (
+        <>
+          <div className="fixed inset-0 z-40 bg-black bg-opacity-20 backdrop-blur-sm" onClick={onClick} />
+          <HeaderMenu />
+        </>
+      )}
     </div>
   );
 };
