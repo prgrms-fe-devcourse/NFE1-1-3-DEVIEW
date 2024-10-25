91d5fe07d82f715ccf8b215bba804283819f5f36
Arthur Jo <ioi456859@gmail.com>
2024-10-25 11:11:45 +0900

    Feat: PostInputContainer UI 구현

--- src/components/PostCreatePage/PostInputContainer.tsx
+++ src/components/PostCreatePage/PostInputContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 type questionProps = {
   category: string;
   explain: string;
@@ -5,12 +6,24 @@ type questionProps = {
 };
 
 export const PostInputContainer = ({ category, explain, placeholder }: questionProps) => {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
   return (
     <div className="w-full rounded-lg border-2 border-solid border-gray py-7 pl-3 pr-6">
       <div className="flex flex-col gap-3">
         <h2 className="text-24 font-semibold">{category}</h2>
         <p className="text-20 font-medium text-primary">{explain}</p>
-        <input className="text-gray" type="text" placeholder={placeholder} />
+        <input
+          value={value}
+          onChange={onChange}
+          className="text-gray"
+          type="text"
+          placeholder={placeholder}
+          defaultChecked
+        />
       </div>
     </div>
   );
