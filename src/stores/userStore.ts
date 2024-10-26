6e4fe60660426f771448e83b282c496a72d2b32f
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-26 15:59:10 +0900

    Feat: userStore isLoggedIn 추가

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -3,12 +3,14 @@ import { create } from "zustand";
 
 type UserStore = {
   userInfo: UserInfo | null;
+  isLoggedIn: boolean;
   setUserInfo: (userInfo: UserInfo) => void;
   clearUserInfo: () => void;
 };
 
 export const useUserStore = create<UserStore>((set) => ({
   userInfo: null,
-  setUserInfo: (userInfo: UserInfo) => set({ userInfo }),
-  clearUserInfo: () => set({ userInfo: null })
+  isLoggedIn: false,
+  setUserInfo: (userInfo: UserInfo) => set({ userInfo, isLoggedIn: true }),
+  clearUserInfo: () => set({ userInfo: null, isLoggedIn: false })
 }));
