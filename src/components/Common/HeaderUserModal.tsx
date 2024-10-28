4d1614f1d841c8772772cede19968639b4749672
jaja <dkwjdals35@gmail.com>
2024-10-28 13:45:41 +0900

    style: HeaderUserModal svg를 react-icons로 변경

--- src/components/Common/HeaderUserModal.tsx
+++ src/components/Common/HeaderUserModal.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-
+import { CiUser } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
 type HeaderUserModalProps = {
   toggleUserIconModal: () => void;
 };
@@ -13,16 +14,7 @@ export const HeaderUserModal = ({ toggleUserIconModal }: HeaderUserModalProps) =
           to="/mypage"
           onClick={toggleUserIconModal}
         >
-          <svg
-            className="hidden md:block"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="25"
-            viewBox="0 0 24 25"
-          >
-            <circle cx="12" cy="8" r="5" fill="gray" />
-            <path d="M12 15c-4 0-7 2.5-7 5v2h14v-2c0-2.5-3-5-7-5z" fill="gray" />
-          </svg>
+          <CiUser className="hidden md:block md:h-6 md:w-6" />
           마이페이지
         </Link>
 
@@ -30,26 +22,7 @@ export const HeaderUserModal = ({ toggleUserIconModal }: HeaderUserModalProps) =
           className="text-10 block w-full px-4 py-2 flex-center hover:opacity-50 md:text-20"
           onClick={toggleUserIconModal}
         >
-          <svg
-            className="hidden md:block"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="25"
-            fill="none"
-            viewBox="0 0 24 25"
-          >
-            <g clip-path="url(#clip0_48_146)">
-              <path
-                fill="gray"
-                d="M17 7.35001L15.59 8.76001L18.17 11.35H8V13.35H18.17L15.59 15.93L17 17.35L22 12.35L17 7.35001ZM4 5.35001H12V3.35001H4C2.9 3.35001 2 4.25001 2 5.35001V19.35C2 20.45 2.9 21.35 4 21.35H12V19.35H4V5.35001Z"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_48_146">
-                <rect width="24" height="24" fill="white" transform="translate(0 0.350006)" />
-              </clipPath>
-            </defs>
-          </svg>
+          <IoIosLogOut className="mr-1 hidden md:block" />
           로그아웃
         </button>
       </div>
