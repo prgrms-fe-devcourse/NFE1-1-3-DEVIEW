3fe5c44c8137fe6c331daa85bfc2f37a84dbb262
axl0926 <song22092000@gmail.com>
2024-10-28 12:46:36 +0900

    Chore:vite-plugin-svgr 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   css: {
     postcss: {
       plugins: [tailwindcss()]
