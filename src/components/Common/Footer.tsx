72245232f1593204f48497d13124190163b13869
Commeci <cjtkemfldj@naver.com>
2024-10-28 10:21:30 +0900

    Feat: 로고 클릭 시 main으로

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { Logo } from "@components/Common/Logo";
 
 export default function Footer() {
   return (
     <footer className="h-40 bg-black px-16 py-7">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <p className="mt-8 text-12 text-white-sub">ⓒ2024. deview all rights reserved</p>
     </footer>
   );
