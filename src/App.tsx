1647c2fca6277cf79108d325130a6306b81a3585
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-28 12:22:21 +0900

    Fix: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { router } from "@/router";
 import { getUserInfo } from "@services/auth/getUserInfo";
-import { getUserPosts } from "@services/post/getUserPosts";
 import { useUserStore } from "@stores/userStore";
 import { AccessTokenStorage } from "@utils/localStorage";
 import { useEffect } from "react";
@@ -15,7 +14,6 @@ export const App = () => {
       getUserInfo().then((data) => {
         setUserInfo(data);
       });
-      getUserPosts({ page: 1, limit: 10 }).then((data) => console.log(data));
     }
   }, []);
 
