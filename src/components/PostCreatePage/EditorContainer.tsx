170999ba1e8fa590b5d087bf2561a4effb0367fb
joarthvr <ioi456859@gmail.com>
2024-10-28 11:48:57 +0900

    Feat: editorContainer 컴포넌트 quill 라이브러리 사용 변경

--- src/components/PostCreatePage/EditorContainer.tsx
+++ src/components/PostCreatePage/EditorContainer.tsx
@@ -1,22 +1,73 @@
-import "@toast-ui/editor/dist/toastui-editor.css";
+import { useState, useMemo } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-import { Editor } from "@toast-ui/react-editor";
+type ToolbarOptions =
+  | string
+  | number
+  | boolean
+  | { header: (number | boolean)[] }
+  | { list: string }
+  | { script: string }
+  | { indent: string };
+
+interface CustomModule {
+  toolbar: ToolbarOptions[][];
+}
 
 export const EditorContainer = () => {
+  const [value, setValue] = useState("");
+
+  const modules = useMemo(
+    () =>
+      ({
+        toolbar: [
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic", "underline", "strike"],
+          ["code", "code-block"],
+          ["blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ script: "sub" }, { script: "super" }],
+          [{ indent: "-1" }, { indent: "+1" }],
+          ["link"],
+          ["clean"]
+        ]
+      }) as CustomModule,
+    []
+  );
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "code",
+    "code-block",
+    "blockquote",
+    "list",
+    "bullet",
+    "script",
+    "indent",
+    "link"
+  ];
+
   return (
     <div className="flex w-full flex-col gap-4 rounded-lg border-2 border-solid border-gray py-7 pl-3 pr-6">
       <div className="flex flex-col gap-3">
         <h2 className="text-24 font-semibold">질문하고 싶은 코드를 작성해주세요</h2>
         <p className="text-20 font-medium text-primary">주석으로 설명을 추가해주세요.</p>
-
-        <Editor
-          initialValue="질문하시고 싶은 코드 또는 내용을 작성해주세요."
-          previewStyle="vertical"
-          height="80vh"
-          initialEditType="markdown"
-          useCommandShortcut={true}
-          className="max-w-sm"
-        />
+        <div className="h-[80vh]">
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            modules={modules}
+            formats={formats}
+            placeholder="코드를 입력해주세요"
+            className="h-[95%]"
+          />
+        </div>
       </div>
     </div>
   );
