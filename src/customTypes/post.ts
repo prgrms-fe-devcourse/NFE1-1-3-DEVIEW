849f3a11ae639c2c21ad1485ba97feb1f5eb8e0c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-28 11:04:02 +0900

    Feat: getPosts

---
+++ src/customTypes/post.ts
@@ -0,0 +1,29 @@
+export type Post = {
+  _id: string;
+  title: string;
+  content: string;
+  author: {
+    _id: string;
+    username: string;
+  };
+  devDependencies: string[];
+  likesCount: number;
+  viewsCount: number;
+  scrapsCount: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+export type CommonPostRequestProps = {
+  postId: string;
+  page: number;
+  limit: number;
+};
+export type CommonPostResponseProps = {
+  posts: Post[];
+  scraps: Post[];
+  currentPage: number;
+  totalPages: number;
+  totalPosts: number;
+};
