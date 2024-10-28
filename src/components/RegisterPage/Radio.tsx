8d01d109254b8fc27a7cd8319035fa9461c5e13f
Commeci <cjtkemfldj@naver.com>
2024-10-28 09:25:55 +0900

    Fix: conflic 발생 부분 돌림

--- src/components/RegisterPage/Radio.tsx
+++ src/components/RegisterPage/Radio.tsx
@@ -3,16 +3,16 @@ type RadioProps = {
   name: string;
   id: string;
   value: string;
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "type">;
+};
 
-export const Radio = ({ text, name, id, value, ...rest }: RadioProps) => {
+export const Radio = ({ text, name, id, value }: RadioProps) => {
   return (
     <label
       className="mb-5 inline-block w-fit flex-1 cursor-pointer rounded-lg p-6 text-center ring-1 ring-lightgray transition-shadow checked:bg-primary hover:ring-primary has-[:checked]:ring-secondary"
       htmlFor={id}
     >
       {text}
-      <input id={id} type="radio" name={name} value={value} hidden {...rest} />
+      <input id={id} type="radio" name={name} value={value} />
     </label>
   );
 };
