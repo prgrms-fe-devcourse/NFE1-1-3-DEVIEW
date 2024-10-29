4848accd1c56e62187189da796c717730999739b
Commeci <cjtkemfldj@naver.com>
2024-10-29 18:03:27 +0900

    Style: max-w 설정

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -3,11 +3,13 @@ import { Logo } from "@components/Common/Logo";
 
 export default function Footer() {
   return (
-    <footer className="h-40 bg-black px-16 py-7">
-      <Link to="/">
-        <Logo />
-      </Link>
-      <p className="mt-8 text-12 text-white-sub">ⓒ2024. deview all rights reserved</p>
+    <footer className="h-40 bg-black py-7">
+      <div className="m-4 max-w 2xl:m-auto">
+        <Link to="/">
+          <Logo />
+        </Link>
+        <p className="mt-8 text-12 text-white-sub">ⓒ2024. deview all rights reserved</p>
+      </div>
     </footer>
   );
 }
