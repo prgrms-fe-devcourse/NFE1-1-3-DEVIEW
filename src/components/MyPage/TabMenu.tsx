6320523386604c0baa6235be009b0b52508c03d4
Commeci <cjtkemfldj@naver.com>
2024-10-29 16:06:09 +0900

    Style: 임시 데이터 삭제

--- src/components/MyPage/TabMenu.tsx
+++ src/components/MyPage/TabMenu.tsx
@@ -1,45 +1,11 @@
 import { CommentsContent } from "@components/MyPage/CommentsContent";
 import { InfoContent } from "@components/MyPage/InfoContent";
-import { LikesContent } from "@components/MyPage/LikesContent";
+import { ScrapContent } from "@components/MyPage/ScrapContent";
 import { PostsContent } from "@components/MyPage/PostsContent";
-import { TPost } from "@customTypes/post";
-import { UserInfo } from "@customTypes/userInfo";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const TabMenu = () => {
   const [activeTab, setActiveTab] = useState("posts");
-
-  // TODO: 유저 타입 처리하기
-  // TODO: 임시데이터, 백엔드랑 연결하기
-  const [likesList, setLikesList] = useState<TPost[]>([]);
-  const [infoList, setInfoList] = useState<UserInfo | null>(null);
-
-  useEffect(() => {
-    setLikesList([
-      {
-        _id: "2",
-        title: "React Router를 사용해 navigate하는 방법이 뭔가요?",
-        content: "React Router를 사용하여 페이지 간 이동을 구현하고 싶습니다. 어떻게 해야 할까요?",
-        author: {
-          _id: "author2",
-          username: "홍길서"
-        },
-        devDependencies: ["React", "JavaScript"],
-        likesCount: 35,
-        viewsCount: 1234,
-        scrapsCount: 8,
-        commentsCount: 5,
-        createdAt: "2024-10-23",
-        updatedAt: "2024-10-24",
-        __v: 0
-      }
-    ]);
-    setInfoList({
-      id: "user123",
-      username: "홍길동",
-      group: "개발자"
-    });
-  }, []);
 
   const renderContent = () => {
     switch (activeTab) {
@@ -48,9 +14,9 @@ export const TabMenu = () => {
       case "comments":
         return <CommentsContent />;
       case "likes":
-        return <LikesContent data={likesList} />;
+        return <ScrapContent />;
       case "info":
-        return infoList ? <InfoContent data={infoList} /> : null;
+        return <InfoContent />;
       default:
         return null;
     }
