3315226a06a39ff3ce26508d22643b965dcc3d40
Commeci <cjtkemfldj@naver.com>
2024-10-29 11:45:34 +0900

    Feat: CommentsContent 컴포넌트 api 연결

--- src/components/MyPage/CommentsContent.tsx
+++ src/components/MyPage/CommentsContent.tsx
@@ -1,14 +1,44 @@
+import { Loading } from "@components/Common/Loading";
 import { MyComment } from "@components/MyPage/MyComment";
+import { NoUserContent } from "@components/MyPage/NoUserContent";
 import { TComment } from "@customTypes/comment";
+import { getUserComments } from "@services/comment/getUserComments";
+import { useEffect, useState } from "react";
 
-type CommentsContentProps = {
-  data: TComment[];
-};
+export const CommentsContent = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [commentList, setCommentList] = useState<TComment[]>([]);
 
-export const CommentsContent = ({ data }: CommentsContentProps) => {
+  useEffect(() => {
+    const fetchComments = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await getUserComments();
+        setCommentList(response.comments);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "댓글 정보를 불러올 수 없습니다");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchComments();
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="flex">
+        <Loading />
+      </div>
+    );
+  if (error) return <div>Error: {error}</div>;
+  if (commentList.length === 0) return <NoUserContent type="comment" />;
+
   return (
     <div className="">
-      {data.map((comment) => (
+      {commentList.map((comment) => (
         <MyComment key={comment._id} comment={comment} />
       ))}
     </div>
