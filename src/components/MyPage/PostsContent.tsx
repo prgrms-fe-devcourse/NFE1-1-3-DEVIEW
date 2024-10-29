54735f93d552a108c6cb6dd0470dc72b183cd75f
Commeci <cjtkemfldj@naver.com>
2024-10-29 16:05:26 +0900

    Style: 스타일 코드, 주석, 경로 수정

--- src/components/MyPage/PostsContent.tsx
+++ src/components/MyPage/PostsContent.tsx
@@ -1,11 +1,12 @@
 import { Loading } from "@components/Common/Loading";
 import { PostListItem } from "@components/Common/PostListItem";
-import { NoUserContent } from "@components/MyPage/NoUserContent";
+import { NoContent } from "@components/Common/NoContent";
 import { TPost } from "@customTypes/post";
 import { getUserPosts } from "@services/post/getUserPosts";
 import { useQuery } from "@tanstack/react-query";
 
 export const PostsContent = () => {
+  // TODO: 파라미터 수정? 필요
   const { data, isLoading, error } = useQuery<{ posts: TPost[] }, Error>({
     queryKey: ["userPosts"],
     queryFn: () =>
@@ -20,14 +21,14 @@ export const PostsContent = () => {
 
   if (isLoading)
     return (
-      <div className="flex">
+      <div className="flex h-screen items-center justify-center">
         <Loading />
       </div>
     );
 
   if (error) return <div>Error: {error.message}</div>;
 
-  if (!data || data.posts.length === 0) return <NoUserContent type="post" />;
+  if (!data || data.posts.length === 0) return <NoContent type="post" />;
 
   return (
     <div className="">
