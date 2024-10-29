2a127ae2a802de4fcc46cfb2745252a2f92cccda
Commeci <cjtkemfldj@naver.com>
2024-10-29 16:03:36 +0900

    Refactor: 경로, 스타일, query코드 수정

--- src/components/MyPage/CommentsContent.tsx
+++ src/components/MyPage/CommentsContent.tsx
@@ -1,25 +1,26 @@
 import { Loading } from "@components/Common/Loading";
+import { NoContent } from "@components/Common/NoContent";
 import { MyComment } from "@components/MyPage/MyComment";
-import { NoUserContent } from "@components/MyPage/NoUserContent";
+import { CommonCommentResponseProps } from "@customTypes/comment";
 import { getUserComments } from "@services/comment/getUserComments";
 import { useQuery } from "@tanstack/react-query";
 
 export const CommentsContent = () => {
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading, error } = useQuery<CommonCommentResponseProps, Error>({
     queryKey: ["userComments"],
-    queryFn: getUserComments
+    queryFn: () => getUserComments({ page: 1, limit: 10 })
   });
 
   if (isLoading)
     return (
-      <div className="flex">
+      <div className="flex h-screen items-center justify-center">
         <Loading />
       </div>
     );
 
   if (error) return <div>Error: {(error as Error).message}</div>;
 
-  if (!data || data.comments.length === 0) return <NoUserContent type="comment" />;
+  if (!data || data.comments.length === 0) return <NoContent type="comment" />;
 
   return (
     <div className="">
