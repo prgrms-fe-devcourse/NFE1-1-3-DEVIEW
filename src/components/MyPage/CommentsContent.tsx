b6d522d5cb2fd1970335cb185be0dddfef0cdedd
Commeci <cjtkemfldj@naver.com>
2024-10-29 12:48:09 +0900

    Feat: react-query로 api 연결

--- src/components/MyPage/CommentsContent.tsx
+++ src/components/MyPage/CommentsContent.tsx
@@ -1,31 +1,14 @@
 import { Loading } from "@components/Common/Loading";
 import { MyComment } from "@components/MyPage/MyComment";
 import { NoUserContent } from "@components/MyPage/NoUserContent";
-import { TComment } from "@customTypes/comment";
 import { getUserComments } from "@services/comment/getUserComments";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export const CommentsContent = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const [commentList, setCommentList] = useState<TComment[]>([]);
-
-  useEffect(() => {
-    const fetchComments = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await getUserComments();
-        setCommentList(response.comments);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : "댓글 정보를 불러올 수 없습니다");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchComments();
-  }, []);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["userComments"],
+    queryFn: getUserComments
+  });
 
   if (isLoading)
     return (
@@ -33,12 +16,14 @@ export const CommentsContent = () => {
         <Loading />
       </div>
     );
-  if (error) return <div>Error: {error}</div>;
-  if (commentList.length === 0) return <NoUserContent type="comment" />;
+
+  if (error) return <div>Error: {(error as Error).message}</div>;
+
+  if (!data || data.comments.length === 0) return <NoUserContent type="comment" />;
 
   return (
     <div className="">
-      {commentList.map((comment) => (
+      {data.comments.map((comment) => (
         <MyComment key={comment._id} comment={comment} />
       ))}
     </div>
