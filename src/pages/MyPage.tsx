29e26e61aa667078820b0dc748b07730c8a074f3
Commeci <cjtkemfldj@naver.com>
2024-10-29 10:23:49 +0900

    Feat: getUserInfo api 연결

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,18 +1,33 @@
 import { TabMenu } from "@components/MyPage/TabMenu";
 import { UserProfile } from "@components/MyPage/UserProfile";
 import { UserInfo } from "@customTypes/userInfo";
+import { getUserInfo } from "@services/auth/getUserInfo";
 import { useEffect, useState } from "react";
 
 export default function MyPage() {
-  // TODO: api 연결하기 (유저 정보, 내 게시글, 내 댓글, 스크랩)
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
-    setUserInfo({
-      id: "test1212",
-      username: "홍길동",
-      group: "개발자"
-    });
+    const fetchUserInfo = async () => {
+      try {
+        const data = await getUserInfo();
+        setUserInfo(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("유저 정보 가져오기 실패");
+        }
+      }
+    };
+
+    fetchUserInfo();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="mx-4 flex max-w flex-col gap-6 py-12 md:gap-12 xl:mx-auto">
