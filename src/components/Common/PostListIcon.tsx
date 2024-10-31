a027c274667d2c64c794e00e72efd3e6bf5b884d
Commeci <cjtkemfldj@naver.com>
2024-10-31 17:12:14 +0900

    Style: icon 이미지 repeaet 제거, 사이즈 고정 cover

--- src/components/Common/PostListIcon.tsx
+++ src/components/Common/PostListIcon.tsx
@@ -6,8 +6,8 @@ type PostListIconProps = {
 export const PostListIcon = ({ devDependencies }: PostListIconProps) => {
   const iconUrl = devIconsDocs[devDependencies]?.iconUrl || "bg-[url(/assets/dev_icons/Default.svg)]";
   return (
-    <>
-      <div className={`h-14 w-14 bg-contain ${iconUrl}`}></div>
-    </>
+    <div className="flex-shrink-0">
+      <div className={`h-10 w-10 bg-cover bg-center bg-no-repeat md:h-14 md:w-14 ${iconUrl}`}></div>
+    </div>
   );
 };
