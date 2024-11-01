f173c09c9e610764571d5c5b289d3a03dd414f84
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-01 12:30:26 +0900

    Feat: UserProfile Avatar 적용

--- src/components/MyPage/UserProfile.tsx
+++ src/components/MyPage/UserProfile.tsx
@@ -1,19 +1,15 @@
 import { UserInfo } from "@customTypes/userInfo";
+import Avatar from "boring-avatars";
 
 type UserProfileProps = {
   userInfo: UserInfo;
 };
 
 export const UserProfile = ({ userInfo }: UserProfileProps) => {
-  // TODO: 유저 프로필 사진 처리하기
   return (
     <div className="flex items-center gap-4">
       <div className="h-16 w-16 overflow-hidden rounded-full md:h-24 md:w-24">
-        <img
-          className="h-auto w-full"
-          src="https://media.istockphoto.com/id/1012645084/ko/%EB%B2%A1%ED%84%B0/%EC%99%84%EB%B2%BD-%ED%95%9C-%EB%9E%9C%EB%8D%A4-%ED%8C%A8%ED%84%B4-%EB%B2%A1%ED%84%B0.jpg?s=170667a&w=0&k=20&c=_fZKK0-ZyFFLungr9E06AOz8r_M4h8aHYLtU2cEJ-yA="
-          alt="프로필 이미지"
-        />
+        <Avatar name={userInfo.userId} variant="beam" />
       </div>
       <span className="text-16 text-secondary md:text-20">
         {userInfo.username}
