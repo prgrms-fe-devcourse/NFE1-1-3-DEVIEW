213b5d8e21f55bbb143d8e22f63823203a8b07cf
axl0926 <song22092000@gmail.com>
2024-11-01 14:28:23 +0900

    Feat:MainSection Swiper추가 및 대체 컴포넌트 추가

--- src/components/MainPage/MainSection.tsx
+++ src/components/MainPage/MainSection.tsx
@@ -1,17 +1,56 @@
-import { MainBannerWrap } from "@components/MainPage/MainBannerWrap";
 import { Suspense } from "react";
 import { MainBannerSkeleton } from "@components/MainPage/MainBannerSkeleton";
 import { ErrorBoundary } from "react-error-boundary";
+import MainBanner from "@components/MainPage/MainBanner";
+import { BestReviewerBanner } from "@components/MainPage/BestReviewerBanner";
+import { MostViewedPostBanner } from "@components/MainPage/MostViewedPostBanner";
+import { QuestionBanner } from "@components/MainPage/QuestionBanner";
+import { MostViewedTodayPostsBanner } from "@components/MainPage/mostViewedTodayPostsBanner";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Pagination } from "swiper/modules";
+
+type bannersType = {
+  banner: React.ComponentType;
+  color: "bg-secondary" | "bg-purple" | "bg-skyblue" | "bg-lightgreen";
+};
 export const MainSection = () => {
+  const banners: bannersType[] = [
+    { banner: QuestionBanner, color: "bg-secondary" },
+    { banner: MostViewedPostBanner, color: "bg-purple" },
+    { banner: MostViewedTodayPostsBanner, color: "bg-skyblue" },
+    { banner: BestReviewerBanner, color: "bg-lightgreen" }
+  ];
   return (
     <div className="bg-lightpurple flex-center">
-      <div className="flex max-w gap-10 px-10 py-12">
-        <ErrorBoundary fallbackRender={({ error }) => <MainBannerSkeleton type="error" error={error.message} />}>
-          <Suspense fallback={<MainBannerSkeleton type="loading" />}>
-            <MainBannerWrap />
-          </Suspense>
-        </ErrorBoundary>
-      </div>
+      <Swiper
+        modules={[Pagination]}
+        className="max-w px-10 py-12"
+        spaceBetween={40}
+        slidesPerView={1}
+        breakpoints={{
+          768: {
+            slidesPerView: 2
+          },
+          1024: {
+            slidesPerView: 3
+          },
+          1280: {
+            slidesPerView: 4
+          }
+        }}
+      >
+        {banners.map(({ banner, color }) => (
+          <SwiperSlide className="transition-transform hover:scale-105">
+            <ErrorBoundary fallback={<MainBannerSkeleton isError={true} />}>
+              <Suspense fallback={<MainBannerSkeleton />}>
+                <MainBanner color={color}>{React.createElement(banner)}</MainBanner>
+              </Suspense>
+            </ErrorBoundary>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
