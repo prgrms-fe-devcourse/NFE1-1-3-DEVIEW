05a7b37fc823fa6dfca5de30f8d1f178ef1941b2
Commeci <cjtkemfldj@naver.com>
2024-11-01 14:35:33 +0900

    Feat: CodeViewer 적용

--- src/components/MyPage/MyComment.tsx
+++ src/components/MyPage/MyComment.tsx
@@ -1,3 +1,4 @@
+import { CodeViewer } from "@components/PostDetailPage";
 import { TComment } from "@customTypes/comment";
 import { useNavigate } from "react-router-dom";
 
@@ -15,9 +16,10 @@ export const MyComment = ({ comment }: MyCommentProps) => {
       className="group flex cursor-pointer flex-col gap-2 border-b-2 border-solid border-primary border-opacity-40 py-2 md:gap-4 md:py-4"
       onClick={onClick}
     >
-      <p className="truncate text-justify text-14 text-black md:text-16">{comment.content}</p>
+      {/* <p className="truncate text-justify text-14 text-black md:text-16">{comment.content}</p> */}
+      <CodeViewer content={comment.content} />
       <p>
-        <span className="text-16 text-secondary group-hover:underline md:text-20">{comment.postTitle}</span>
+        <span className="break-words text-16 text-secondary group-hover:underline md:text-20">{comment.postTitle}</span>
         <span className="text-14 text-black md:text-16"> 에 남긴 댓글</span>
       </p>
       <p className="text-12 text-gray md:text-16">{comment.createdAt}</p>
