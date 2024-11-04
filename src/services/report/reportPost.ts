6921169e0c971bcf553e5d779b6759fca8f824b6
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-04 14:49:13 +0900

    Feat: reportPost, getHiddenPosts

---
+++ src/services/report/reportPost.ts
@@ -0,0 +1,41 @@
+import { ErrorResponse } from "@customTypes/errorResponse";
+import axiosInstance from "@services/axiosInstance";
+import { AccessTokenStorage } from "@utils/localStorage";
+import axios, { AxiosError } from "axios";
+
+type ReportPostRequestProps = {
+  postId: string;
+  reason: string;
+};
+
+type ReportPostResponseProps = {
+  message: string;
+};
+
+export async function reportPost({ postId, reason }: ReportPostRequestProps): Promise<ReportPostResponseProps> {
+  try {
+    const response = await axiosInstance.post<ReportPostResponseProps>(
+      `/report/${postId}`,
+      { reason },
+      {
+        headers: {
+          Authorization: AccessTokenStorage.getAuthorizationHeader()
+        }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError<ErrorResponse>;
+      if (axiosError.response) {
+        console.error("게시물 신고 실패", axiosError.response.data);
+        throw new Error(axiosError.response.data.message || "요청 실패");
+      } else if (axiosError.request) {
+        console.error("요청 에러:", axiosError.request);
+        throw new Error("서버에 연결할 수 없습니다. 네트워크를 확인해 주세요.");
+      }
+    }
+    console.error("예상치 못한 에러 발생:", error);
+    throw new Error("예상치 못한 에러가 발생했습니다. 다시 시도해 주세요.");
+  }
+}
