93955b84d17b9b33a0d94e4ddbb311ebd867bda3
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-04 17:49:21 +0900

    Fix: register request props

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -35,7 +35,8 @@ export default function RegisterPage() {
         group,
         userId,
         password,
-        username
+        username,
+        role: "user"
       })
         .then(() => {
           navigate("/login"); // 성공 시 로그인 페이지로 리다이렉트
