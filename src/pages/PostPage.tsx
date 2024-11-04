84e47be9d6541f2da115a0c9261c6fd7b28db814
axl0926 <song22092000@gmail.com>
2024-11-04 16:15:41 +0900

    Feat:PostPage 추가"

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,3 +1,27 @@
+import { PostPageHeader } from "@components/PostPage/PostPageHeader";
+import { PostPageList } from "@components/PostPage/PostPageList";
+import { PostPageSkeleton } from "@components/PostPage/PostPageListSkeleton";
+import { Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { useParams } from "react-router-dom";
+
 export default function PostPage() {
-  return <div>PostPage</div>;
+  const { id } = useParams<{ id: string }>();
+  const [order, setOrder] = useState<"recent" | "popular">("recent");
+  return (
+    <>
+      <div className="m-auto max-w p-20">
+        {id ? (
+          <PostPageHeader id={id} order={order} setOrder={setOrder} />
+        ) : (
+          <PostPageHeader order={order} setOrder={setOrder} />
+        )}
+        <ErrorBoundary fallback={<PostPageSkeleton isError={true} />}>
+          <Suspense fallback={<PostPageSkeleton />}>
+            {id ? <PostPageList order={order} userId={id} /> : <PostPageList order={order} />}
+          </Suspense>
+        </ErrorBoundary>
+      </div>
+    </>
+  );
 }
