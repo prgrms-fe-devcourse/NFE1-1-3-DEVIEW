fba24f33f14514b62ceca48c61664f7870e0da67
jaja <dkwjdals35@gmail.com>
2024-11-05 15:34:53 +0900

    Style: 유저랭킹 스타일 변경

--- src/components/RankPage/RankListItem.tsx
+++ src/components/RankPage/RankListItem.tsx
@@ -11,8 +11,8 @@ export const RankListItem = ({ rank, userId, _id, team, recommend }: RankListIte
     <div className="flex items-center justify-between py-4 text-12 leading-10 md:text-16">
       <span className="flex-shrink-0 flex-grow-0 basis-1/12 text-left font-bold text-primary">{rank}</span>
       <div className="flex flex-shrink-0 flex-grow-0 basis-3/12 items-center space-x-2 overflow-hidden">
-        <Avatar name={userId ?? ""} variant="beam" className="h-4 w-4 sm:h-8 sm:w-8 md:h-12 md:w-12" />
-        <span className="overflow-hidden text-ellipsis whitespace-nowrap text-center">{userId}</span>
+        <Avatar name={userId ?? ""} variant="beam" className="h-6 w-6 sm:h-8 sm:w-8 md:h-12 md:w-12" />
+        <span className="w-28 overflow-hidden text-ellipsis whitespace-nowrap text-center">{userId}</span>
       </div>
       <span className="flex-shrink-0 flex-grow-0 basis-2/12 text-center font-bold text-gray">{team}</span>
       <span className="flex-shrink-0 flex-grow-0 basis-2/12 text-right font-bold text-gray">추천수 {recommend}</span>
