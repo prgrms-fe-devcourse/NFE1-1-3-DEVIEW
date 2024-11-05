3959004370630e661ce3252b3d1f4a4286739708
jaja <dkwjdals35@gmail.com>
2024-11-05 12:33:50 +0900

    Style: 유저랭킹페이지 스타일 수정

--- src/components/RankPage/RankHeader.tsx
+++ src/components/RankPage/RankHeader.tsx
@@ -1,14 +1,3 @@
 export const RankHeader = () => {
-  return (
-    <thead>
-      <tr className="text-12 font-bold leading-10 md:text-16">
-        <th className="items-center justify-center border-b border-solid border-black px-8 py-4 text-left">Rank</th>
-        <th className="items-center justify-center border-b border-solid border-black px-4 py-4">아이디</th>
-        <th className="items-center justify-center border-b border-solid border-black px-4 py-4">소속</th>
-        <th className="items-center justify-center border-b border-solid border-black px-4 py-4 text-right md:px-8">
-          추천수
-        </th>
-      </tr>
-    </thead>
-  );
+  return <h2 className="mb-0 text-14 font-bold text-gray md:mb-4 md:text-20">종합 랭킹</h2>;
 };
