3a2632176ae17a67bc383adb5449f338b2dbeab0
Commeci <cjtkemfldj@naver.com>
2024-11-06 14:40:16 +0900

    Style: calc 수정

--- src/components/MyPage/InfoContent.tsx
+++ src/components/MyPage/InfoContent.tsx
@@ -88,7 +88,7 @@ export const InfoContent = () => {
   }
 
   return (
-    <div className="flex min-h-[calc(100vh-20rem)] w-full items-center justify-center">
+    <div className="flex min-h-[calc(100vh-28rem)] w-full items-center justify-center">
       <div className="w-full max-w-[768px] p-4 md:m-auto">
         {!isVerified ? (
           <CheckPassword onSuccess={() => setIsVerified(true)} />
