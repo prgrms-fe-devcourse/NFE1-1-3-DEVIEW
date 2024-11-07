f2f6c5bdc2fb8dea0485f107120f759361f1d14b
axl0926 <song22092000@gmail.com>
2024-11-07 10:39:28 +0900

    Fix:메인 배너에서 오류 발생 시 에러메시지를 출력

--- src/components/MainPage/MainBannerSkeleton.tsx
+++ src/components/MainPage/MainBannerSkeleton.tsx
@@ -1,6 +1,6 @@
-type MainBannerSkeletonProps = { isError?: boolean };
+type MainBannerSkeletonProps = { isError?: boolean; error?: string };
 
-export const MainBannerSkeleton = ({ isError = false }: MainBannerSkeletonProps) => {
+export const MainBannerSkeleton = ({ isError = false, error }: MainBannerSkeletonProps) => {
   const bgColor = isError ? "bg-pink " : "bg-lightgray animate-pulse";
-  return <div className={`flex h-52 rounded-lg ${bgColor} shadow`}></div>;
+  return <div className={`flex h-52 rounded-lg ${bgColor} p-4 text-16 text-white-sub shadow`}>{error}</div>;
 };
